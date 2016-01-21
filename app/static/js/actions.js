8129a0e99c209bce4b16b60f33facb437c66a3a5
Alexandros Nikiforidis <anikiforidis@simplon.co>
2016-01-21 16:01:21 +0100

    register custom events defore/after ajax submit

--- app/static/js/actions.js
+++ app/static/js/actions.js
@@ -3,10 +3,20 @@ jQuery(document).ready(function() {
         changeActiveContext.change(jQuery(this));
     });
 
-
     jQuery(ajaxFormHandler.forms).on('submit', function(e) {
         e.preventDefault();
 
         ajaxFormHandler.send(jQuery(this));
-    })
+    });
+
+    jQuery(document).on('ajaxSubmitStarted', function(e) {
+        e.preventDefault();
+        jQuery('#loading-modal').modal('show');
+    });
+
+    jQuery(document).on('ajaxSubmitFinished', function(e) {
+        e.preventDefault();
+        jQuery('#loading-modal').modal('hide');
+    });
 });
+
